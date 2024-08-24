b356f836f48baf808825c82aea0c7940f668eedf
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-08-24 15:43:12 +0530

    add nav-bar button for small-screen  devices

--- index-js-files/nav-bar.js
+++ index-js-files/nav-bar.js
@@ -1,10 +1,4 @@
-
-
-document.getElementById('navToggle').addEventListener('click', function() {
-    var navContent = document.getElementById('navContent');
-    if (navContent.classList.contains('active')) {
-        navContent.classList.remove('active');
-    } else {
-        navContent.classList.add('active');
-    }
-});
+function toggleMenu() {
+    const navContent = document.querySelector('.nav-content');
+    navContent.classList.toggle('active');
+}
